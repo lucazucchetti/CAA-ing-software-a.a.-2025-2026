9d5f62c0e61c256049e69e01dc275e4d1ceec994
lucazucchetti <l.zucchetti3@studenti.unibg.it>
2026-02-04 10:29:18 +0100

    pulizia commento per gli import

--- chatbypics/lib/main.dart
+++ chatbypics/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:chatbypics/screens/authPage.dart';
 import 'package:chatbypics/screens/Authentication/verifyEmailPage.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Importa Firestore
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
